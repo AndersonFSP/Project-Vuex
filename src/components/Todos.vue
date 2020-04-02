e21264e03e41201040898e15d912f259a7eecebb
Anderson Filipe <anderson.lemobs@gmail.com>
2020-04-01 23:31:03 -0300

    Projeto inicio

---
+++ src/components/Todos.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3>Todos</h3>
+        <h4>{{msg}}</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Todos",
+    props:{
+        msg: String
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
