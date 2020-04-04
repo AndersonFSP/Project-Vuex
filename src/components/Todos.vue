4475a5570d181706aa9b03aa91b476f313174e08
Anderson Filipe <anderson.lemobs@gmail.com>
2020-04-04 17:10:56 -0300

    Remover Todo

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -4,6 +4,7 @@
         <div class="todos">
             <div v-for="todo in allTodos" :key="todo.id" class="todo">
                 {{todo.title}}
+                <i @click='deleteTodo(todo.id)'><font-awesome-icon icon="trash"/></i>
             </div>
         </div>
     </div>
@@ -14,7 +15,7 @@ import { mapGetters, mapActions } from 'vuex';
 export default {
     name: "Todos", 
     methods: {
-        ...mapActions(['fetchTodos']),
+        ...mapActions(['fetchTodos', 'deleteTodo']),
     },
     computed: mapGetters(['allTodos']),
     created(){
@@ -38,5 +39,13 @@ export default {
         text-align: center;
         position: relative;
         cursor: pointer ;
+    }
+
+    i{
+        position: absolute;
+        color: rgba(255, 0, 0, 0.582);
+        bottom: 10px;
+        right:10px;
+        cursor:pointer;
     }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
 import todos from './modules/todos';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faUserSecret, faTrash)
 
 // load Vuex
 Vue.use(Vuex);
 
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 // create store
 export default new Vuex.Store({
