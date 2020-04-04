167b967c49bb3bcb01492d1981cf0ef2a6cdd0e4
Anderson Filipe <anderson.lemobs@gmail.com>
2020-04-04 19:24:27 -0300

    Editar Todo

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,17 @@
 <template>
     <div>
         <h3>Todos</h3>
+        <div class="legend">
+            <span>Double click to mark as complete</span>
+            <span>
+                <span class="incomplete-box"></span> = Incomplete
+            </span>
+            <span>
+                <span class="complete-box"></span> = Complete
+            </span>
+        </div>
         <div class="todos">
-            <div v-for="todo in allTodos" :key="todo.id" class="todo">
+            <div @dblclick="onDblclick(todo)" v-for="todo in allTodos" :key="todo.id" class="todo" :class="{'is-complete':todo.completed}">
                 {{todo.title}}
                 <i @click='deleteTodo(todo.id)'><font-awesome-icon icon="trash"/></i>
             </div>
@@ -15,7 +24,15 @@ import { mapGetters, mapActions } from 'vuex';
 export default {
     name: "Todos", 
     methods: {
-        ...mapActions(['fetchTodos', 'deleteTodo']),
+        ...mapActions(['fetchTodos', 'deleteTodo', "updateTodo"]),
+        onDblclick(todo){
+            const updTodo = {
+                id: todo.id,
+                title: todo.title,
+                completed: !todo.completed
+            } 
+            this.updateTodo(updTodo);
+        }
     },
     computed: mapGetters(['allTodos']),
     created(){
@@ -47,5 +64,32 @@ export default {
         bottom: 10px;
         right:10px;
         cursor:pointer;
+    }
+    .complete-box{
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        background: #35495e;
+    }
+    .incomplete-box{
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        background: #41b883;
+    }
+    .is-complete {
+        background: #35495e;
+        color: #ccc;
+    }
+    .legend{
+        display: flex;
+        justify-content: space-around;
+        margin-bottom: 1rem;
+    }
+    @media (max-width: 500px) {
+        .todos { 
+            grid-template-columns: 1fr;
+        }
+        
     }
 </style>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -27,16 +27,27 @@ const actions = {
     },
 
     async filterTodos({ commit }, limit){
-        console.log(limit);
        const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`)
        commit('setTodos', response.data);
-    }    
+    },
+
+    async updateTodo({ commit }, updTodo){
+        const response = await axios.put(`https://jsonplaceholder.typicode.com/todos/${updTodo.id}`, updTodo);
+        commit('updateTodo', response.data);
+        console.log(response.data)
+    }
 };
 
 const  mutations = {
     setTodos: (state, todos) => {(state.todos = todos)},
     newTodo: (state, todo) => state.todos.unshift(todo),
-    removeTodo: (state, id)=>  state.todos = state.todos.filter(todo => todo.id !== id)
+    removeTodo: (state, id)=>  state.todos = state.todos.filter(todo => todo.id !== id),
+    updateTodo: (state, updTodo) => {
+        const index = state.todos.findIndex(todo => todo.id === updTodo.id);
+        if( index !== -1 ) {
+            state.todos.splice(index, 1, updTodo);
+        }
+    }
 };
 
 export default {
